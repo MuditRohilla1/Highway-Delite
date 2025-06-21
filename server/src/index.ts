0ee141f632eadf7a593e02627623eeb17eb3a018
muditrohilla <rohillamudit679@gmail.com>
2025-06-21 08:50:34 +0530

    added port for deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 app.use("/api/user", userRoutes)
@@ -18,6 +20,6 @@ app.use("/api/user", userRoutes)
 mongoose
     .connect(process.env.MONGO_URI!)
     .then(() => {
-        app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+        app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
     })
     .catch((err) => console.error("DB Error", err));
